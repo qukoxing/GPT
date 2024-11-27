a7b50ec6745dbf65b50fbb5a6e8a9b2eb3de78da
qukoxing <qukoxing@gmail.com>
2024-11-27 23:28:46 +0800

    2024-11-27 23:28:46

--- main.py
+++ main.py
@@ -333,3 +333,4 @@ class EnglishNumberNormalizer:
             "trillion": 1_000_000_000_000,
             "quadrillion": 1_000_000_000_000_000,
             "quintillion": 1_000_000_000_000_000_000,
+            "sextillion": 1_000_000_000_000_000_000_000,
