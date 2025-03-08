551e2f75f52fd8bf2b9deef3abd135067be0e2b0
qukoxing <qukoxing@gmail.com>
2025-03-09 02:09:29 +0800

    2025-03-09 02:09:28

--- main.py
+++ main.py
@@ -390,3 +390,4 @@ class EnglishNumberNormalizer:
             "sixes" if name == "six" else name + "s": (value, "s")
             for name, value in self.ones.items()
         }
+        self.ones_ordinal = {
