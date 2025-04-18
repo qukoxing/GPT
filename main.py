281474c1c1ea70e0ceeff24a83b9f96656c20b0e
qukoxing <qukoxing@gmail.com>
2025-04-18 14:38:55 +0800

    2025-04-18 14:38:54

--- main.py
+++ main.py
@@ -419,3 +419,4 @@ class EnglishNumberNormalizer:
             name.replace("y", "ies"): (value, "s") for name, value in self.tens.items()
         }
         self.tens_ordinal = {
+            name.replace("y", "ieth"): (value, "th")
